e49c5e0e1ae476d197c9649375bc6f197229da7f
Caleb Manor <CMANOR@harding.edu>
2021-02-24 12:04:44 -0600

    First commit

---
+++ comp3100p1.py
@@ -0,0 +1,20 @@
+# Caleb Manor
+
+# This is a Python comment
+# Create some dynamically typed variables and intitialize them
+
+an_integer = 55
+a_string = "This is a Python program"
+a_bool = True
+a_float = 3.25
+
+# Here is a calculation
+
+the_answer = an_integer * a_float + ord(a_string[0]) + int(a_bool)
+print(the_answer)
+
+# Write code that will output an_integer to the fourth power
+# output the_answer to one decimal place
+
+print(pow(an_integer, 4))
+print(round(the_answer, 1))
